42323def49c416a857e60386e27d071ab317e29a
Alpha Coma <400829150120@r3-1.deped.gov.ph>
2023-08-22 21:39:35 +0800

    feat(config): add dynamicStartUrl & dynamicStartUrlRedirect

--- next.config.js
+++ next.config.js
@@ -23,6 +23,8 @@ const withPWA = require('next-pwa')({
   buildExcludes: [/middleware-manifest.json$/],
   skipWaiting: true,
   scope: '/',
+  dynamicStartUrl: true,
+  dynamicStartUrlRedirect: '/auth/signin',
   sw: 'service-worker.js',
   disable: process.env.NODE_ENV === 'development',
 });
